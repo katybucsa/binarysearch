9f1964e388141ebb617fc44331e9ae94d4cf4475
Katy <katybucsa@yahoo.com>
2022-09-15 20:55:18 +0300

    remove multiple spaces

--- Weekly Contest 56/problem_1.cpp
+++ Weekly Contest 56/problem_1.cpp
@@ -2,22 +2,21 @@
 
 bool solve(std::string s) {
 
-	int occ[10] = { 0 };
+  int occ[10] = { 0 };
 
-	for (int i = 0; i < s.size(); i++)
-		occ[s[i] - '0']++;
+  for (int i = 0; i < s.size(); i++)
+    occ[s[i] - '0']++;
 
+  bool found_pair = false;
+  for (int i = 0; i < 10; i++) {
+    if (occ[i] % 3 != 0 && (occ[i] - 2) % 3 != 0)
+      return false;
 
-	bool found_pair = false;
-	for (int i = 0; i < 10; i++) {
-		if (occ[i] % 3 != 0 && (occ[i] - 2) % 3 != 0)
-			return false;
-
-		if (occ[i] > 1 && (occ[i] - 2) % 3 == 0)
-			if (found_pair)
-				return false;
-			else
-				found_pair = true;
-	}
-	return found_pair;
+    if (occ[i] > 1 && (occ[i] - 2) % 3 == 0)
+      if (found_pair)
+        return false;
+      else
+        found_pair = true;
+  }
+  return found_pair;
 }
