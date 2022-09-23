55453be4e40d95456f116c7d3c0517d63106a5bc
Katy <katybucsa@yahoo.com>
2022-09-23 16:10:35 +0300

    add problem 1 requirements

--- problem_1.cpp
+++ problem_1.cpp
@@ -1,5 +1,12 @@
-#include <vector>
+﻿#include <vector>
 #include <set>
+
+/**
+* Given a list of numbers nums and a number k, return whether any two elements from the list add up to k. You may not use the same element twice.
+* Note: Numbers can be negative or 0.
+* Constraints
+* n ≤ 100,000 where n is the length of nums
+*/
 
 bool solve(std::vector<int>& nums, int k) {
   std::set<int> distinct;
