521bee2cfa8f05d30d3fd040296879af45274a66
Katy <katybucsa@yahoo.com>
2022-09-23 16:08:11 +0300

    problem 2

---
+++ problem_2.cpp
@@ -0,0 +1,21 @@
+﻿#include <string>
+#include <stack>
+
+/**
+* You're given a string s consisting solely of "(" and ")". Return whether the parentheses are balanced.
+* Constraints
+* n ≤ 100,000 where n is the length of s.
+*/
+
+bool solve(std::string s) {
+
+  std::stack<char> parentheses;
+  for (auto c : s)
+  {
+    if (c == '(')
+      parentheses.push(c);
+    else if (!parentheses.empty())
+      parentheses.pop();
+  }
+  return parentheses.empty();
+}
