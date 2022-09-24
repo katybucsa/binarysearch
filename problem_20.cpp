e45c3ec436ec7137eed2ec4245fe37dbc2bd0775
Katy <katybucsa@yahoo.com>
2022-09-24 14:14:18 +0300

    problem 20

---
+++ problem_20.cpp
@@ -0,0 +1,23 @@
+﻿#include <vector>
+
+/*
+* Given a list of integers nums, return whether the largest number is bigger than the second-largest number by more than two times.
+* Constraints
+* 2 ≤ n ≤ 100,000 where n is the length of nums
+*/
+
+bool solve(std::vector<int>& nums) {
+
+  int max1 = nums[0], max2 = nums[0];
+
+  for (auto n : nums) {
+    if (n > max1)
+      max1 = n;
+  }
+
+  for (auto n : nums) {
+    if ((n > max2 || max2 == max1) && n != max1)
+      max2 = n;
+  }
+  return max1 > 2 * max2;
+}
