12f9162b76c5967e244cfaf3f1680eaec61710ea
Katy <katybucsa@yahoo.com>
2022-09-27 16:46:21 +0300

    problem 39

---
+++ problem_39.cpp
@@ -0,0 +1,22 @@
+﻿
+
+/**
+ * Given a singly linked list node, return its length. The linked list has fields next and val.
+ * Constraints
+ * n ≤ 100,000 where n is the number of nodes in node
+ */
+class LLNode {
+public:
+  int val;
+  LLNode* next;
+};
+
+int solve(LLNode* node) {
+
+  int length = 0;
+  while (node != nullptr) {
+    length++;
+    node = node->next;
+  }
+  return length;
+}
