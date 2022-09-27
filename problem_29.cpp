7b2ed5a63bd4636319f2992079be4960e04b280f
Katy <katybucsa@yahoo.com>
2022-09-27 16:10:01 +0300

    problem 29

---
+++ problem_29.cpp
@@ -0,0 +1,22 @@
+﻿#include <string>
+
+/*
+* Given a string lowercase alphabet s, eliminate consecutive duplicate characters from the string and return it.
+* That is, if a list contains repeated characters, they should be replaced with a single copy of the character. The order of the elements should not be changed.
+* Constraints
+* 0 ≤ n ≤ 100,000 where n is the length of s
+*/
+std::string solve(std::string s) {
+
+  if (s.size() < 2)
+    return s;
+
+  std::string res;
+  for (int i = 0; i < s.size() - 1; i++)
+  {
+    if (s[i] != s[i + 1])
+      res += s[i];
+  }
+  res += s[s.size() - 1];
+  return res;
+}
