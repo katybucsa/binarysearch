3df4b1fc2d32f5a024cc315ac4e439b6713a4c7a
Katy <katybucsa@yahoo.com>
2022-09-27 16:02:19 +0300

    problem 28

---
+++ problem_28.cpp
@@ -0,0 +1,17 @@
+﻿#include <string>
+
+/*
+* Given a lowercase alphabet string s, determine whether it has all unique characters.
+* Constraints
+* 0 ≤ n ≤ 100,000 where n is the length of s
+*/
+bool solve(std::string s) {
+
+  int a[256] = { 0 };
+  for (char c : s)
+    if (a[c] != 0)
+      return false;
+    else
+      a[c]++;
+  return true;
+}
