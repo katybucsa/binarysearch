78766581b536c3bc451aa72135aebaa375442efd
Katy <katybucsa@yahoo.com>
2022-09-27 18:23:48 +0300

    problem 41

---
+++ problem_41.cpp
@@ -0,0 +1,34 @@
+﻿/*
+* Given a singly linked list node, return its reverse.
+* Bonus: Can you do this in \mathcal{O}(1)O(1) space?
+* Constraints
+* n ≤ 100,000 where n is the number of nodes in node
+*/
+
+class LLNode {
+public:
+  int val;
+  LLNode* next;
+
+  LLNode(int val, LLNode* next) : val(val), next(next) {}
+};
+
+
+LLNode* solve(LLNode* node) {
+
+  if (node->next == nullptr)
+    return node;
+
+  LLNode* f = node;
+  LLNode* s = node->next;
+  f->next = nullptr;
+
+  while (s != nullptr)
+  {
+    node = s;
+    s = s->next;
+    node->next = f;
+    f = node;
+  }
+  return f;
+}
