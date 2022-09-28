2dc854113ec0b6ae4b73b30673232caaa672f24b
Katy <katybucsa@yahoo.com>
2022-09-28 10:38:48 +0300

    problem 93

---
+++ problem_93.cpp
@@ -0,0 +1,24 @@
+﻿/*
+* Given the head of a singly linked list head, return whether the values of the nodes are sorted in a strictly increasing order.
+* Constraints
+* 1 ≤ n ≤ 100,000 where n is the number of nodes in head
+*/
+
+class LLNode {
+public:
+  int val;
+  LLNode* next;
+};
+
+bool solve(LLNode* head) {
+
+  int prev_val = head->val;
+  while (head->next != nullptr)
+  {
+    head = head->next;
+    if (head->val <= prev_val)
+      return false;
+    prev_val = head->val;
+  }
+  return true;
+}
