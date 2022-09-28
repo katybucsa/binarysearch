fe15ba3440c2f9a807c7a82d088a3e3d38632977
Katy <katybucsa@yahoo.com>
2022-09-28 10:59:48 +0300

    problem 203

---
+++ problem_203.cpp
@@ -0,0 +1,41 @@
+﻿/*
+* Given a singly linked list node, and an integer target, return the same linked list with all nodes whose value is target removed.
+* Constraints
+* n ≤ 100,000 where n is the number of nodes in node
+*/
+class LLNode {
+public:
+  int val;
+  LLNode* next;
+};
+
+LLNode* solve(LLNode* node, int target) {
+
+  LLNode* prev = nullptr;
+  LLNode* first = node;
+  LLNode* temp;
+  while (node != nullptr)
+  {
+    if (node->val == target)
+    {
+      temp = node;
+      if (prev == nullptr)
+      {
+        node = node->next;
+        first = node;
+      }
+      else
+      {
+        prev->next = node->next;
+        node = node->next;
+      }
+      delete temp;
+    }
+    else
+    {
+      prev = node;
+      node = node->next;
+    }
+  }
+  return first;
+}
