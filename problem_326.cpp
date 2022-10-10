d878e6c818139cba638cdf0eb6fe89fc56c6d194
Katy <katybucsa@yahoo.com>
2022-10-10 17:05:16 +0300

    problem 326

---
+++ problem_326.cpp
@@ -0,0 +1,22 @@
+﻿#include <vector>
+#include <set>
+
+/*
+* Given a two dimensional list of integers votes, where each list has two elements [candidate_id, voter_id], report whether any voter has voted more than once.
+* Constraints
+* n ≤ 100,000 where n is the length of votes
+*/
+
+
+bool solve(std::vector<std::vector<int>>& votes) {
+
+  std::set<int> voters;
+
+  for (auto vote : votes)
+  {
+    if (voters.find(vote[1]) != voters.end())
+      return true;
+    voters.insert(vote[1]);
+  }
+  return false;
+}
