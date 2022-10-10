e8fc2c7843e3e236477e1909c6a3e0a07b3bcd69
Katy <katybucsa@yahoo.com>
2022-10-10 17:03:11 +0300

    problem 61

---
+++ problem_61.cpp
@@ -0,0 +1,20 @@
+﻿#include <string>
+#include <set>
+
+/*
+* Given a lowercase alphabet string s, return the index of the first recurring character in it. If there are no recurring characters, return -1.
+* Constraints
+* n ≤ 100,000 where n is the length of s
+*/
+
+int solve(std::string s) {
+
+  std::set<char> characters;
+  for (int i = 0; i < s.size(); i++)
+  {
+    if (characters.find(s[i]) != characters.end())
+      return i;
+    characters.insert(s[i]);
+  }
+  return -1;
+}
